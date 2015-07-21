aa85d81c2d0e583247dbd9232c44325dbce844a6
Jamis Buck <jamisbuck@gmail.com>
2015-07-21 08:34:24 -0600

    bit of refactoring

--- lib/fcpm-ruby.rb
+++ lib/fcpm-ruby.rb
@@ -2,98 +2,97 @@ require 'bundler'
 require 'yaml'
 require 'open-uri'
 
-module BundlerWrap
+module FCPM
 
   def self.config
     @config ||= YAML.load_file(".fcpm-ruby-config") rescue {}
   end
 
-end
+  module CachedInstall
 
-module CachedInstall
+    def explicit_name
+      saved_platform, self.platform = self.platform, Gem::Platform.local
+      full_name
+    ensure
+      self.platform = saved_platform
+    end
 
-  def explicit_name
-    saved_platform, self.platform = self.platform, Gem::Platform.local
-    full_name
-  ensure
-    self.platform = saved_platform
+    def explicit_file_name
+      explicit_name + ".tar.gz"
+    end
+
+    def cached_file
+      File.join(Gem.dir, "cache", explicit_file_name)
+    end
+
+    def cached_file_exists?
+      File.exists?(cached_file)
+    end
+
   end
 
-  def explicit_file_name
-    explicit_name + ".tar.gz"
-  end
+  module FetchPrecompiledGem
 
-  def cached_file
-    File.join(Gem.dir, "cache", explicit_file_name)
-  end
+    def fetch_gem(spec)
+      if !spec.cached_file_exists? && FCPM.config['source']
+        uri = URI.parse(File.join(FCPM.config['source'], spec.explicit_file_name))
 
-  def cached_file_exists?
-    File.exists?(cached_file)
-  end
+        begin
+          body = uri.read
+          cache_dir = File.dirname(spec.cached_file)
+          FileUtils.mkdir_p(cache_dir)
+          File.open(spec.cached_file, "wb") { |f| f.write(body) }
+        rescue OpenURI::HTTPError
+          puts "no remote cached instance at #{uri}"
+        end
+      end
 
-end
-
-module FetchPrecompiledGem
-
-  def fetch_gem(spec)
-    if !spec.cached_file_exists? && BundlerWrap.config['source']
-      uri = URI.parse(File.join(BundlerWrap.config['source'], spec.explicit_file_name))
-
-      begin
-        body = uri.read
-        cache_dir = File.dirname(spec.cached_file)
-        FileUtils.mkdir_p(cache_dir)
-        File.open(spec.cached_file, "wb") { |f| f.write(body) }
-      rescue OpenURI::HTTPError
-        puts "no remote cached instance at #{uri}"
+      if spec.cached_file_exists?
+        # extract gem file from cached install
+        system("tar -xzf #{spec.cached_file} -C #{Gem.dir} cache/#{spec.full_name}.gem")
+        File.join(Gem.dir, "cache", spec.full_name + ".gem")
+      else
+        super
       end
     end
 
-    if spec.cached_file_exists?
-      # extract gem file from cached install
-      system("tar -xzf #{spec.cached_file} -C #{Gem.dir} cache/#{spec.full_name}.gem")
-      File.join(Gem.dir, "cache", spec.full_name + ".gem")
-    else
-      super
-    end
   end
 
-end
+  module InstallPrecompiledGem
 
-module InstallPrecompiledGem
+    def install
+      if !spec.cached_file_exists?
+        old_install_dir = @install_dir
+        old_gem_home = @gem_home
 
-  def install
-    if !spec.cached_file_exists?
-      old_install_dir = @install_dir
-      old_gem_home = @gem_home
+        temp_path = "/tmp/fcpm-ruby-#{spec.name}.build"
+        FileUtils.rm_rf(temp_path)
+        FileUtils.mkdir_p(temp_path)
 
-      temp_path = "/tmp/.fcpm-ruby-#{spec.name}.tmp"
-      FileUtils.rm_rf(temp_path)
-      FileUtils.mkdir_p(temp_path)
+        @install_dir = @gem_home = temp_path
+        super
+        @install_dir, @gem_home = old_install_dir, old_gem_home
 
-      @install_dir = @gem_home = temp_path
-      super
-      @install_dir, @gem_home = old_install_dir, old_gem_home
+        cache_dir = File.dirname(spec.cached_file)
+        FileUtils.mkdir_p(cache_dir)
+        system "tar -czf #{spec.cached_file} -C #{temp_path} ."
+        FileUtils.rm_rf(temp_path)
+      end
 
-      cache_dir = File.dirname(spec.cached_file)
-      FileUtils.mkdir_p(cache_dir)
-      system "tar -czf #{spec.cached_file} -C #{temp_path} ."
-      FileUtils.rm_rf(temp_path)
+      if spec.cached_file_exists?
+        FileUtils.mkdir_p(@install_dir)
+        system "tar -xzf #{spec.cached_file} -C #{@install_dir}"
+      else
+        # FIXME: something went wrong?
+        abort "something died?"
+      end
+
+      spec
     end
 
-    if spec.cached_file_exists?
-      FileUtils.mkdir_p(@install_dir)
-      system "tar -xzf #{spec.cached_file} -C #{@install_dir}"
-    else
-      # FIXME: something went wrong?
-      abort "something died?"
-    end
-
-    spec
   end
-
 end
 
-Gem::BasicSpecification.send :prepend, CachedInstall
-Bundler::Source::Rubygems.send :prepend, FetchPrecompiledGem
-Bundler::GemInstaller.send :prepend, InstallPrecompiledGem
+Gem::BasicSpecification.send :prepend, FCPM::CachedInstall
+Bundler::Source::Rubygems.send :prepend, FCPM::FetchPrecompiledGem
+Bundler::GemInstaller.send :prepend, FCPM::InstallPrecompiledGem
